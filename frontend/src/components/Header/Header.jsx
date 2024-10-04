25c4d6d101e8cb6b3c34e092cf0d3a55f6203990
Amalia Kivansa Anggreansi <72345090+amaliakivansa@users.noreply.github.com>
2024-10-04 13:44:46 +0700

    feat: add header component

---
+++ frontend/src/components/Header/Header.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import './Header.css';
+
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="header-contents">
+        <h2>Order your favorite food here</h2>
+        <p>
+          choose from a diverse menu featuring a delectable array pf dishes
+          crafted with the finest ingredients and culinary expertise. Our
+          mission is to satisfy your cravings and make your dining experience
+          memorable, one delicious meal at time.
+        </p>
+        <button>View Menu</button>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
